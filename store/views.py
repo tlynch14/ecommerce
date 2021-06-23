5354723d241dc9032ffd52a02b579e3d6cea071c
Thomas Lynch <Thomas.lynch931@gmail.com>
2021-06-23 11:26:56 +0100

    Adding simple html homepage

--- store/views.py
+++ store/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from .models import Category, Product
+
+
+def all_products(request):
+    products = Product.objects.all()
+    return render(request, 'store/home.html', {'products': products})
